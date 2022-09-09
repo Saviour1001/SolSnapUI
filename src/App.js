f95caeedccc19d5c5c34ea463c3365c57b8e458c
Saviour1001 <harshghodkar@gmail.com>
2022-09-09 12:14:47 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const { ethereum } = window;
+  const snapId = "npm:solanasnap2";
+  async function something() {
+    const result = await ethereum.request({
+      method: "wallet_enable",
+      params: [
+        {
+          wallet_snap: { [snapId]: { version: "0.0.1" } },
+        },
+      ],
+    });
+    console.log(result);
+  }
+  async function someWork() {
+    const hello = await ethereum.request({
+      method: "wallet_invokeSnap",
+      params: [
+        "npm:@anamansari062/solanasnap2",
+        { method: "getExtendedPublicKey" },
+      ],
+    });
+
+    console.log(hello);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={something}>Hello</button>
+      <button onClick={someWork}>Work</button>
     </div>
   );
 }
