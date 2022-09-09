fa70fd31cb6d82b457b3c9466c281e909a6a5749
Anam Ansari <anamansari062@gmail.com>
2022-09-09 18:06:40 +0530

    Added QR code for public key

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from 'react';
+import QRCode from 'react-qr-code';
 
 function App() {
     const { ethereum } = window;
     // const snapId = "npm:solanasnap2";
+    const [value, setValue] = useState();
+    const [back, setBack] = useState('#FFFFFF');
+    const [fore, setFore] = useState('#000000');
+    const [size, setSize] = useState(256);
+
     async function something() {
         try {
             const result = await ethereum.request({
@@ -19,6 +26,7 @@ function App() {
                     eth_accounts: {},
                 }, ],
             });
+
             console.log(result);
 
         } catch (error) {
@@ -30,8 +38,6 @@ function App() {
                 console.log('Unexpected error:', error);
             }
         }
-
-        
     }
     async function someWork() {
         const hello = await ethereum.request({
@@ -41,12 +47,30 @@ function App() {
                 { method: "getPublicExtendedKey" },
             ],
         });
-
+        const hello1 = await ethereum.request({
+            method: "wallet_invokeSnap",
+            params: [
+                'npm:solanasnap1',
+                { method: "getPublicKey" },
+            ],
+        });
+        setValue(hello1);
         console.log(hello);
+        console.log(hello1);
+
     }
     return ( <div className = "App" >
         <button onClick = { something } > Hello </button> 
-        <button onClick = { someWork } > Work </button> 
+        <button onClick = { someWork } > Work </button>
+        {value && (
+          <QRCode
+            title="Receive"
+            value={value}
+            bgColor={back}
+            fgColor={fore}
+            size={size === '' ? 0 : size}
+          />
+        )}
         </div>
     );
 }
