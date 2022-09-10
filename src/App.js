23b0dbf33f50e18da34dab4b04ad3a380cd1fcd0
Anam Ansari <anamansari062@gmail.com>
2022-09-10 17:08:02 +0530

    Show balance and Send sols

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useState } from 'react';
 import QRCode from 'react-qr-code';
+import { SystemProgram, PublicKey, Connection, Keypair, LAMPORTS_PER_SOL, clusterApiUrl, Transaction } from "@solana/web3.js";
+import * as bs58 from "bs58";
+
+// connection
+const connection = new Connection(clusterApiUrl('devnet'), 'confirmed');;
 
 function App() {
     const { ethereum } = window;
     // const snapId = "npm:solanasnap2";
-    const [value, setValue] = useState();
+    const [value, setValue] = useState({pubKey: "", balance: 0
+    });
     const [back, setBack] = useState('#FFFFFF');
     const [fore, setFore] = useState('#000000');
     const [size, setSize] = useState(256);
 
-    async function something() {
+    async function installSnap() {
         try {
             const result = await ethereum.request({
                 method: 'wallet_enable',
@@ -39,37 +45,74 @@ function App() {
             }
         }
     }
-    async function someWork() {
-        const hello = await ethereum.request({
+    async function connectToWallet() {
+        const keypair = await ethereum.request({
             method: "wallet_invokeSnap",
             params: [
                 'npm:solanasnap1',
                 { method: "getPublicExtendedKey" },
             ],
         });
-        const hello1 = await ethereum.request({
+        const pubKey = await ethereum.request({
             method: "wallet_invokeSnap",
             params: [
                 'npm:solanasnap1',
                 { method: "getPublicKey" },
             ],
         });
-        setValue(hello1);
-        console.log(hello);
-        console.log(hello1);
+        const bal = await connection.getBalance(new PublicKey(pubKey));
+
+        let updatedValue = {};
+        updatedValue = { pubKey: pubKey, balance: bal / LAMPORTS_PER_SOL};
+        setValue(value => ({
+            ...value,
+            ...updatedValue
+            }));
+
+        console.log(keypair);
+        console.log(pubKey);
+        console.log(`${bal / LAMPORTS_PER_SOL} SOL`);
 
     }
-    return ( <div className = "App" >
-        <button onClick = { something } > Hello </button> 
-        <button onClick = { someWork } > Work </button>
-        {value && (
+
+    async function sendSol(){
+        let tx = new Transaction();
+        tx.add(
+            SystemProgram.transfer({
+            fromPubkey: value.pubKey,
+            toPubkey: new PublicKey("GdNh12yVy5Lsew9WXVCV5ErgK5SpmsBJkcti5jVtPB7o"),
+            lamports: 1 * LAMPORTS_PER_SOL,
+            })
+        );
+        const send = await ethereum.request({
+            method: "wallet_invokeSnap",
+            params: [
+                'npm:solanasnap1',
+                { method: "signTransaction",
+                params: tx },
+            ],
+        });
+        console.log(send);
+    }
+
+    return ( 
+    <div className = "App" >
+        <button onClick = { installSnap } > Install Snap </button> 
+        <button onClick = { connectToWallet } > Connect </button>
+        
+        {(value.pubKey != "") && (
+        <div>
           <QRCode
             title="Receive"
-            value={value}
+            value={value.pubKey}
             bgColor={back}
             fgColor={fore}
             size={size === '' ? 0 : size}
           />
+          <text id="sol">{value.balance} Sols</text>
+          <button onClick = { sendSol } > Send Sol </button>
+          </div>
+          
         )}
         </div>
     );
